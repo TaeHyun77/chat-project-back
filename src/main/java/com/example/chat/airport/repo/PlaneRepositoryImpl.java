3c52617b4c48e5a78a92562bd594740ab305fabf
park <acd2283@gmail.com>
2025-04-02 03:14:04 +0900

    feat Plane repo Query DSL 적용 및 항공편 데이터 saveAll 변경

---
+++ src/main/java/com/example/chat/airport/repo/PlaneRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.example.chat.airport.repo;
+
+import com.example.chat.airport.entity.QPlane;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class PlaneRepositoryImpl implements PlaneRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public long deleteByScheduleDateStartsWith(String today) {
+
+        return queryFactory
+                .delete(QPlane.plane)
+                .where(QPlane.plane.scheduleDatetime.startsWith(today)
+                        .and(QPlane.plane.remark.eq("출발")))
+                .execute();
+    }
+}
